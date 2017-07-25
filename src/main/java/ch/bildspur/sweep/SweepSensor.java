685916ca9fc56fd184e88587c2c27ce9bb8748b5
Florian Bruggisser <florian@nexpose.ch>
2017-07-25 12:15:07 +0200

    added multi-threading sweep sample reading

--- src/main/java/ch/bildspur/sweep/SweepSensor.java
+++ src/main/java/ch/bildspur/sweep/SweepSensor.java
@@ -4,6 +4,7 @@ import io.scanse.sweep.SweepDevice;
 import processing.core.PApplet;
 import processing.core.PConstants;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,17 +12,17 @@ public class SweepSensor implements PConstants {
 
     private PApplet parent;
     private SweepDevice device;
+    private Thread sweepThread;
 
-    private boolean isRunning = false;
+    volatile private boolean isRunning = false;
 
-    private List<SensorSample> samples;
+    volatile private List<SensorSample> samples = new ArrayList<>();
+
 
     public SweepSensor(PApplet parent)
     {
         this.parent = parent;
-
-        // register methods
-        parent.registerMethod("pre", this);
+        parent.registerMethod("stop", this);
     }
 
     public void start(String port)
@@ -31,7 +32,7 @@ public class SweepSensor implements PConstants {
 
         device = new SweepDevice(port);
 
-        PApplet.println("waiting for sweep motors...");
+        PApplet.println("Sweep: waiting for motors...");
         while (!device.isMotorReady()) {
             try {
                 Thread.sleep(100);
@@ -40,18 +41,24 @@ public class SweepSensor implements PConstants {
             }
         }
 
-        PApplet.println("running!");
+        PApplet.println("Sweep: running!");
 
         // start device
         device.startScanning();
+
+        // create update thread
+        sweepThread = new Thread(() -> {
+            while(isRunning)
+                updateScans();
+        });
+
+        // start update thread
         isRunning = true;
+        sweepThread.start();
     }
 
-    public void pre()
+    private void updateScans()
     {
-        if(!isRunning)
-            return;
-
         // turn samples to processing sample
         samples = device.nextScan().stream()
                 .map(SensorSample::new).collect(Collectors.toList());
@@ -64,7 +71,17 @@ public class SweepSensor implements PConstants {
 
         device.stopScanning();
         device.close();
+
         isRunning = false;
+
+        // join thread
+        try {
+            sweepThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        PApplet.println("Sweep: stopped!");
     }
 
     public SweepDevice getDevice() {
